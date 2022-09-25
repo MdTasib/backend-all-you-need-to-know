2db000aba87a3ad553118d9e957fee079e5fbe42
MdTasib <ohidulalam442@gmail.com>
2022-09-25 19:28:32 +0600

    Create the Store and Category model

---
+++ models/Store.js
@@ -0,0 +1,48 @@
+// BRAND SCHEMA
+const mongoose = require("mongoose");
+const validator = require("validator");
+const { ObjectId } = mongoose.Schema.Types;
+
+const storeSchema = mongoose.Schema(
+	{
+		name: {
+			type: String,
+			trim: true,
+			required: [true, "Please provide a store name"],
+			lowercase: true,
+			enum: {
+				values: [
+					"chittagong",
+					"dhaka",
+					"khulna",
+					"rongpur",
+					"barishal",
+					"sylhet",
+				],
+				message: "{VALUE} is not a valid name",
+			},
+		},
+		description: String,
+		status: {
+			type: String,
+			enum: ["active", "inactive"],
+			default: "active",
+		},
+		manager: {
+			name: String,
+			contactNumber: String,
+			id: {
+				type: ObjectId,
+				ref: "User",
+			},
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+// MODAL
+const Store = mongoose.model("Brand", storeSchema);
+
+module.exports = Store;
