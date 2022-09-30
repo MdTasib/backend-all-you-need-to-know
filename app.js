367b2091a7f3290d055bc7f51ad8bf41184e3f28
MdTasib <ohidulalam442@gmail.com>
2022-09-30 16:24:43 +0600

    getStore - createStore - getStoreById implete

--- app.js
+++ app.js
@@ -3,17 +3,21 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
 
-// ROUTES
-const productRouter = require("./routes/product.route");
-const brandRouter = require("./routes/brand.route");
-
 // MIDDLEWARES
 app.use(express.json());
 app.use(cors());
 
 // ROUTES
+const productRouter = require("./routes/product.route");
+const brandRouter = require("./routes/brand.route");
+const storeRouter = require("./routes/store.route");
+const categoryRouter = require("./routes/category.route");
+
+// ROUTES USE
 app.use("/api/v1/product", productRouter);
 app.use("/api/v1/brand", brandRouter);
+app.use("/api/v1/store", storeRouter);
+app.use("/api/v1/category", categoryRouter);
 
 app.get("/", (req, res) => {
 	res.send("Route is working! YaY!");
